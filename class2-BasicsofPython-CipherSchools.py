b410a7cdb0e602d1324cfd15d193c0b904878e87
Diptesh0901 <119107331+Diptesh0901@users.noreply.github.com>
2022-11-29 14:28:37 +0530

    Add files via upload

---
+++ class2-BasicsofPython-CipherSchools.py
@@ -0,0 +1,59 @@
+print("Hello")
+
+print("Hello World")
+
+# This is a comment
+
+# Every line executed in python is either
+# 1. Statement
+# 2. Expression
+
+print("Jatin Katyal")
+
+print(100+200)
+
+# Literal Constants
+# 1.integers
+# 2.float
+# 3.string
+# 4.complex
+
+isinstance(print, int)
+
+a = 255
+print(hex(id(a)))
+print(a)
+
+a = "a"
+print(type(a))
+
+a = 200
+print(id(a))
+
+b = 200
+print(id(b))
+
+c = 278
+print(id(c))
+
+a = 'aaaa'
+print(id(a))
+
+b = 'bbbb'
+print(id(b))
+
+a = 3456789098765434
+print(a+1)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ class3-PrintCommandFunctionalities-CipherSchools.py
@@ -0,0 +1,29 @@
+print("Jatin Katyal")
+
+print(1)
+
+# Taking User Input
+# 1.input() function
+# 2.concept of typecasting
+
+# Something more about print functions
+
+print(1, 2, 3, 4, 5, 6, sep="-")
+print(1, 2, 3, "jatin", 3.4, 1+5j, sep=",")
+print("a", end=")")
+print("b")
+print("c")
+
+print("Hello", end=";")
+print("World !")
+
+
+
+
+
+
+
+
+
+
+
